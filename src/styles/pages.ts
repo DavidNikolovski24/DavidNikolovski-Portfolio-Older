b7e873b42986da880d5afae61321a6362e8796c9
David <david.nikolovski.00@gmail.com>
2023-10-12 19:53:01 +0200

    problem

--- src/styles/pages.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const PageStyled = styled.div`
-  height: 100vh;
-  background-color: ${({ theme }) => theme.backgroundColor};
-  * {
-    transition: 1000ms;
-  }
-`;
